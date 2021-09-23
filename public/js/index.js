cb1ac5ed62d6870a0e7806b7ca6d8fbd15e64206
govind <bana2002jwr@gmail.com>
2021-09-23 23:23:01 +0530

    update function included

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,30 @@ $("#create_user").submit(function (event) {
     alert("user added successfully!");
 });
 
+
+$("#update_user").submit(function (event) {
+
+    event.preventDefault();
+    // will store all data on submit the form
+    var unindexed_array = $(this).serializeArray(); //this method will return serialize array of data
+    var data = {} // create a empty object to store the data
+    $.map(unindexed_array, function (n, l) {
+        data[n['name']] = n['value']  // storing data in data array
+    })
+    console.log(data);
+    var request = {
+        "url": `http://localhost:8000/api/users/${data.id}`,
+        "method": "PUT",
+        "data": data
+    } // creating request variable for ajx request
+
+    $.ajax(request).done(function (response) {
+        alert("data updated of student successfully !")
+    })
+
+})
+
+
 if (window.location.pathname == "/") {
     $ondelete = $('.table tbody td a.delete');
     $ondelete.click(function () {
